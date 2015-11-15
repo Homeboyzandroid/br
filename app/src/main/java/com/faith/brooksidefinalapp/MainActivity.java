94b7ec28ce9acaedb05fd4d95196715dece8ef57
Faith Muli <faithmuli33@gmail.com>
2015-11-16 00:17:22 +0300

    registration activity working

--- app/src/main/java/com/faith/brooksidefinalapp/MainActivity.java
+++ app/src/main/java/com/faith/brooksidefinalapp/MainActivity.java
@@ -1,9 +1,13 @@
 package com.faith.brooksidefinalapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.ImageView;
+
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -38,5 +42,21 @@ public class MainActivity extends AppCompatActivity {
         ImageAdapter adapter = new ImageAdapter(getApplicationContext(), web, imageId);
         gridview = (GridView)findViewById(R.id.grid_view);
         gridview.setAdapter(adapter);
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Toast.makeText(getActivity(),"welcome to"+web,Toast.LENGTH_SHORT).show();ra
+
+                switch (position) {
+                    case 0:
+                        startActivity(new Intent(getApplicationContext(), registration.class));
+                        break;
+                    case 1:
+                        startActivity(new Intent(getApplicationContext(), statistics.class));
+                        break;
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/faith/brooksidefinalapp/registration.java
+++ app/src/main/java/com/faith/brooksidefinalapp/registration.java
@@ -1,13 +1,27 @@
 package com.faith.brooksidefinalapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class registration extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        Button button=(Button)findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
